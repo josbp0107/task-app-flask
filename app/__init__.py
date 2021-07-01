1e55029e6f18d1617b601034ec9c3586aebe7669
Jose Ballesteros <josebp0107@gmail.com>
2021-07-01 17:55:58 -0500

    Implementando autenticacion de usuario en login

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,18 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
-
+from flask_login import LoginManager
 from .config import Config
 from .auth import auth
+from .models import UserModel
+
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+
+
+@login_manager.user_loader
+def load_user(username):
+    return UserModel.query(username)
+
 
 def create_app():
     app = Flask(__name__) # Nueva instancia de Flask, donde le tenemos que pasar el nombre de nuestra aplicación, que en este caso seria main.py
@@ -10,6 +20,8 @@ def create_app():
     
     app.config.from_object(Config) # Nos ayuda a generar nuestra llave secreta 
     
+    login_manager.init_app(app)
+
     app.register_blueprint(auth)
 
     return app
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import request, make_response, redirect, render_template, session, url_for, flash
 import unittest
+from flask import request, make_response, redirect, render_template, session, url_for, flash
+from flask_login import login_required
 
 from app import create_app # Importa desde la carpeta app/__init__.py , el metodo create_app
 from app.forms import LoginForm
@@ -36,6 +37,7 @@ def index():
 
 
 @app.route('/hello', methods=['GET'])
+@login_required
 def hello():
     user_ip = session.get('user_ip')
     username = session.get('username')
@@ -46,12 +48,12 @@ def hello():
         'username': username
     }
 
-    users = get_users()
+    # users = get_users()
 
-    for user in users:
-        print(user.id) # Imprime el id por usuario
-        # Convertimos los atributos en diccionarios, ya que las bases de datos NoSQL su estructuras es en forma de documentos
-        print(user.to_dict()['password'])
+    # for user in users:
+    #     print(user.id) # Imprime el id por usuario
+    #     # Convertimos los atributos en diccionarios, ya que las bases de datos NoSQL su estructuras es en forma de documentos
+    #     print(user.to_dict()['password'])
 
     return render_template('hello.html', **context)
 
