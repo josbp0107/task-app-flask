34de603b1435779194ad4a356f45a5ed4b5b9f90
Jose Ballesteros <josebp0107@gmail.com>
2021-07-04 20:47:11 -0500

    Implementando el registro de todos y carga de esta misma

--- app/firestore_service.py
+++ app/firestore_service.py
@@ -15,10 +15,18 @@ def get_user(user_id):
     return db.collection('users').document(user_id).get()
 
 
+def user_put(user_data):
+    """Registrar usuario en la base de datos"""
+    user_ref = db.collection('users').document(user_data.username)
+    user_ref.set({'password': user_data.password})
+
+
 def get_todos(user_id):
+    """Metodo para obtener los todos del usuario"""
     return db.collection('users').document(user_id).collection('todos').get()
 
 
-def user_put(user_data):
-    user_ref = db.collection('users').document(user_data.username)
-    user_ref.set({'password': user_data.password})
+def put_todos(user_id, description):
+    """Este metodo crear todos con un random ID"""
+    todos_collection_ref = db.collection('users').document(user_id).collection('todos')
+    todos_collection_ref.add({'description': description})
--- app/forms.py
+++ app/forms.py
@@ -2,7 +2,13 @@ from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 
+
 class LoginForm(FlaskForm):
     username = StringField('Nombre de usuario', validators=[DataRequired()]) # Inicializamos instancia de DataRequired
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Enviar')
+
+
+class TodoForm(FlaskForm):
+    description = StringField('Descripción', validators=[DataRequired()])
+    submit = SubmitField('Crear')
